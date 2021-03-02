26083dd3c98c9db5e9e4e188ae2cdb1876c1a34c
missi0415 <missi0415@gmail.com>
2021-03-02 14:02:54 +0000

    応用課題6終了

--- bookers2-debug-master/app/controllers/book_comments_controller.rb
+++ bookers2-debug-master/app/controllers/book_comments_controller.rb
@@ -1,22 +1,26 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
+    
   
   end 
 
   def destroy
+    @book = Book.find(params[:book_id])
+    
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    #redirect_to book_path(params[:book_id]) 
+    
     
   end
     
     private
 
   def book_comment_params
-    params.require(:book_comment).permit(:comment)
+    params.require(:book_comment).permit(:comment,:user_id,:book_id)
   end
     
     
