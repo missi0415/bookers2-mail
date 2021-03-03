a8f7228c4810bf6bd2c66eacb39905abda38ef73
missi0415 <76615633+missi0415@users.noreply.github.com>
2021-03-04 01:10:03 +0900

    パス隠す
    パスワードを隠しました

--- bookers2-debug-master/config/environments/development.rb
+++ bookers2-debug-master/config/environments/development.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   :address => 'smtp.gmail.com',
   :port => 587,
   :domain => 'example.com',
-  :user_name => 'missi0415@gmail.com',
-  :password => 'wccrqyfyygwkeeti',
+  :user_name => 'gmailアドレス',
+  :password => 'アプリパスエバーノートに保存',
   :authentication => :plain,
   :enable_starttls_auto => true
 }
